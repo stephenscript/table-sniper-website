efe4711fe2f4713932c85b03b29a9b35ac9b554a
Stephen <stephen.anthony.rivas@gmail.com>
2025-10-19 15:18:23 -0700

    fixed props

--- src/app/r/[city]/[id]/page.tsx
+++ src/app/r/[city]/[id]/page.tsx
@@ -14,5 +14,5 @@ export default async function Page(PageProps: PageProps) {
         redirect('/');
     }
 
-    return <RestaurantHome restaurantData={restaurantData} city={city} id={id} />;
+    return <RestaurantHome restaurantData={restaurantData} />;
 }
